67813647393a285e82e339b6531ab7ce0c672e83
Viranya Gangamina <viranyagangamina@gmail.com>
2025-10-04 20:50:03 +0530

    features added

--- app/src/main/java/com/viranya/fintrack/model/Account.java
+++ app/src/main/java/com/viranya/fintrack/model/Account.java
@@ -1,4 +1,21 @@
 package com.viranya.fintrack.model;
 
 public class Account {
+    private String name;
+    private double balance;
+
+    // Required empty constructor for Firestore
+    public Account() {}
+
+    public Account(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // --- Getters and Setters ---
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
 }
